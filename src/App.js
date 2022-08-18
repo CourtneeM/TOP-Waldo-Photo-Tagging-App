b1773723ea2d951ea1c836aa05d10a426d065577
Courtnee McCollum <courtneemeaghan@gmail.com>
2022-08-18 09:27:54 -0500

    Display character name next to box when user chooses correctly

--- src/App.js
+++ src/App.js
@@ -45,14 +45,32 @@ const Wrapper = styled.div`
       }
     }
   }
+
+  .correct-selection-box {
+    position: absolute;
+    width: 60px;
+    height: 60px;
+    border: 7px solid green;
+    
+    p {
+      position: relative;
+      top: -20px;
+      right: -15px;
+      padding: 2px 5px;
+      width: fit-content;
+      border: 1px solid #000;
+      background-color: #fff;
+      transform: rotate(20deg);
+    }
+  }
 `;
 
 function App() {
   const characterCoordinates = {
-    waldo: [],
-    wizard: null,
-    wanda: null,
-    odlaw: null,
+    waldo: [640, 450],
+    wizard: [755, 450],
+    wanda: [785, 735],
+    odlaw: [290, 455],
   }
 
   const generateBox = (e) => {
@@ -60,26 +78,60 @@ function App() {
     selectionBox.style.display = 'block';
     selectionBox.style.top = `${e.nativeEvent.offsetY - 30}px`;
     selectionBox.style.left = `${e.nativeEvent.offsetX - 30}px`;
-    console.log('x: ' + e.nativeEvent.offsetX, 'y: ' + e.nativeEvent.offsetY);
-    // console.log(e);
+    // console.log('x: ' + e.nativeEvent.offsetX, 'y: ' + e.nativeEvent.offsetY);
+  }
 
-    // waldo coordinates
-    // TL: 605, 425
-    // TR: 665, 425
-    // BL: 605, 485
-    // BR: 665, 485
+  const checkAnswer = (e) => {
+    const [correctX, correctY] = characterCoordinates[e.target.textContent.toLowerCase()];
+    const selectionBox = document.getElementById('selection-box');
+    const minOffsetX = Number(selectionBox.style.left.replace('px', ''));
+    const maxOffsetX = Number(selectionBox.style.left.replace('px', '')) + 80;
+    const minOffsetY = Number(selectionBox.style.top.replace('px', ''));
+    const maxOffsetY = Number(selectionBox.style.top.replace('px', '')) + 80;
+
+    if ((minOffsetX < correctX && correctX < maxOffsetX) && (minOffsetY < correctY && correctY < maxOffsetY)) {
+      // place permanent box
+      const imageContainer = document.querySelector('.image-container');
+      const correctBox = document.createElement('div');
+      correctBox.classList.add('correct-selection-box');
+      correctBox.style.top = selectionBox.style.top;
+      correctBox.style.left = selectionBox.style.left;
+
+      // display character name next to box
+      const characterName = document.createElement('p');
+      characterName.textContent = e.target.textContent;
+
+      correctBox.appendChild(characterName);
+      imageContainer.appendChild(correctBox);
+
+
+      // give user message about correct choice
+      // display message near box for a few seconds  'good job'
+
+      // mark out correct choice in list of characters
+      e.target.style.textDecoration = 'line-through';
+      e.target.style.textColor = '#333';
+      e.target.style.backgroundColor = '#ccc'
+      e.target.style.pointerEvents = 'none';
+    } else {
+      // if answer is wrong, display message 'try again'
+      // remove selectionBox until user clicks again
+
+    }
+    console.log(`offsetX: ${minOffsetX}, ${maxOffsetX}`);
+    console.log(`offsetY: ${minOffsetY}, ${maxOffsetY}`);
   }
 
   return (
     <div>
-      <Wrapper>
+      <Wrapper className='image-container'>
         <img src={beachScene} alt="a" onClick={(e) => generateBox(e)} />
         <div id="selection-box">
           <ul>
-            <li>Waldo</li>
-            <li>Wizard</li>
-            <li>Wanda</li>
-            <li>Odlaw</li>
+            <li onClick={(e) => checkAnswer(e)}>Waldo</li>
+            <li onClick={(e) => checkAnswer(e)}>Wizard</li>
+            <li onClick={(e) => checkAnswer(e)}>Wanda</li>
+            <li onClick={(e) => checkAnswer(e)}>Odlaw</li>
           </ul>
         </div>
       </Wrapper>
